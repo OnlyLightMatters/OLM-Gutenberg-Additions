8deaa2c3595c42487772e95b9edbcf94cabbfe1e
Only Light Matters <xot1643@gmail.com>
2026-07-17 15:58:21 -0300

    Fix Document Module : no opacity could be applied
    Implementing this behaviour would require manipulating Gutenberg's internal DOM or relying on undocumented React internals. This project deliberately avoids such approaches to remain lightweight, stable and compatible with future WordPress releases.

--- modules/document/class-olm-document.php
+++ modules/document/class-olm-document.php
@@ -6,47 +6,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-/**
- * Document module.
- *
- * Adds document-level features to the Block Editor.
- */
 class OLM_GA_Document {
 
-	/**
-	 * Post meta key.
-	 */
 	const META_HIDE_TITLE = '_olm_ga_hide_title';
 
-	/**
-	 * Initialize the module.
-	 *
-	 * @return void
-	 */
 	public static function init() {
 
-		add_action( 'init', [ __CLASS__, 'register_post_meta' ] );
+		add_action( 'init', [ __CLASS__, 'register_meta' ] );
+		add_action( 'enqueue_block_editor_assets', [ __CLASS__, 'enqueue_editor_assets' ] );
 
-		add_action(
-			'enqueue_block_editor_assets',
-			[ __CLASS__, 'enqueue_editor_assets' ]
-		);
-
-		add_filter(
-			'render_block_core/post-title',
-			[ __CLASS__, 'render_post_title' ],
-			10,
-			2
-		);
+		add_filter( 'the_title', [ __CLASS__, 'filter_title' ], 10, 2 );
 
 	}
 
-	/**
-	 * Register post meta.
-	 *
-	 * @return void
-	 */
-	public static function register_post_meta() {
+	public static function register_meta() {
 
 		register_post_meta(
 			'',
@@ -65,20 +38,13 @@ class OLM_GA_Document {
 
 	}
 
-	/**
-	 * Enqueue editor assets.
-	 *
-	 * @return void
-	 */
 	public static function enqueue_editor_assets() {
 
 		wp_enqueue_script(
 			'olm-ga-document',
 			OLM_GA_URL . 'modules/document/olm-document.js',
 			[
-				'wp-block-editor',
 				'wp-components',
-				'wp-compose',
 				'wp-data',
 				'wp-edit-post',
 				'wp-element',
@@ -89,44 +55,27 @@ class OLM_GA_Document {
 			true
 		);
 
-		wp_set_script_translations(
-			'olm-ga-document',
-			'olm-gutenberg-additions',
-			OLM_GA_PATH . 'languages'
-		);
-
-		wp_enqueue_style(
-			'olm-ga-document',
-			OLM_GA_URL . 'modules/document/olm-document.css',
-			[],
-			OLM_GA_VERSION
-		);
-
 	}
 
-	/**
-	 * Hide the Post Title block on the frontend when requested.
-	 *
-	 * @param string $block_content Rendered block.
-	 * @param array  $block         Parsed block.
-	 *
-	 * @return string
-	 */
-	public static function render_post_title( $block_content, $block ) {
+	public static function filter_title( $title, $post_id ) {
 
 		if ( is_admin() ) {
-			return $block_content;
+			return $title;
 		}
 
 		if ( ! is_singular() ) {
-			return $block_content;
+			return $title;
 		}
 
-		if ( get_post_meta( get_the_ID(), self::META_HIDE_TITLE, true ) ) {
+		if ( get_the_ID() !== (int) $post_id ) {
+			return $title;
+		}
+
+		if ( get_post_meta( $post_id, self::META_HIDE_TITLE, true ) ) {
 			return '';
 		}
 
-		return $block_content;
+		return $title;
 
 	}
 
--- modules/document/olm-document.js
+++ modules/document/olm-document.js
@@ -10,58 +10,40 @@
 
 	const META_KEY = '_olm_ga_hide_title';
 
-	function OLM_GA_HideTitlePanel() {
+	function OLM_GA_DocumentPanel() {
 
-		const meta = useSelect((select) => {
-			return select('core/editor').getEditedPostAttribute('meta') || {};
-		}, []);
+		const meta = useSelect(
+			( select ) =>
+				select( 'core/editor' ).getEditedPostAttribute( 'meta' ) || {},
+			[]
+		);
 
-		const { editPost } = useDispatch('core/editor');
-
-		const value = !!meta[META_KEY];
+		const { editPost } = useDispatch( 'core/editor' );
 
 		return el(
 			PluginDocumentSettingPanel,
 			{
 				name: 'olm-ga-document',
-				title: __('Document Properties', 'olm-gutenberg-additions'),
+				title: __( 'Document Properties', 'olm-gutenberg-additions' ),
 			},
-			el(CheckboxControl, {
-				label: __('Hide title on frontend', 'olm-gutenberg-additions'),
-				checked: value,
-				onChange: (checked) => {
-					editPost({
+			el( CheckboxControl, {
+				label: __( 'Hide title on frontend', 'olm-gutenberg-additions' ),
+				checked: !! meta[ META_KEY ],
+				onChange: ( checked ) => {
+					editPost( {
 						meta: {
 							...meta,
-							[META_KEY]: checked,
+							[ META_KEY ]: checked,
 						},
-					});
+					} );
 				},
-			})
+			} )
 		);
 
 	}
 
-	function updateTitleOpacity() {
+	registerPlugin( 'olm-ga-document', {
+		render: OLM_GA_DocumentPanel,
+	} );
 
-		const title = document.querySelector( '.wp-block-post-title' );
-
-		if ( ! title ) {
-			return;
-		}
-
-		if ( wp.data.select( 'core/editor' ).getEditedPostAttribute( 'meta' )[ META_KEY ] ) {
-			title.classList.add( 'olm-ga-hide-title' );
-		} else {
-			title.classList.remove( 'olm-ga-hide-title' );
-		}
-
-	}
-
-	wp.data.subscribe( updateTitleOpacity );
-
-	registerPlugin('olm-ga-document', {
-		render: OLM_GA_HideTitlePanel,
-	});
-
-})(window.wp);
+})( window.wp );
